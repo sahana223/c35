136a87296841245ce982b243f40f6a065481a14a
sahana223 <66789427+sahana223@users.noreply.github.com>
2020-11-20 17:59:23 +0530

    Add files via upload

---
+++ synchronousBallMovement-master/synchronousBallMovement-master/sketch.js
@@ -0,0 +1,18 @@
+var canvas,backgroudImage;
+var gameState=0;
+var playerCount;
+var database;
+
+var form,player,game;
+
+function setup(){
+  canvas = createCanvas(400,400);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+function draw(){
+
+}
